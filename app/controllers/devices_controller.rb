7ade247126a358ce061fb9edc1c1b873ff902784
landlessness <brian@landlessness.net>
2016-01-02 11:48:53 -0500

    refactoring the render stuff out of the controller into the renderer

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -12,47 +12,16 @@ class DevicesController < ApplicationController
   end
   
   def show
-        
-    logger.info device_params
     @device = Device.find(params[:id])
     respond_to do |format|
       format.html # index.html.erb
       format.zetta {
+        # TODO: move headers outta here
         headers['Access-Control-Allow-Origin'] = '*'
         headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
         headers['Access-Control-Request-Method'] = '*'
         headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-        links = []
-        links << [:self, {href: server_device_url(@device.server, @device)}]
-        links << [[:up, 'http://rels.zettaapi.org/server'], {href: server_url(@device.server), title: @device.server.name}]
-        actions = []
-        @device.state_transitions.each do |state_transition|
-          fields = [{
-            name: :action,
-            type: :hidden,
-            value: state_transition.event
-          }]
-          Device.instance_method(state_transition.event).parameters.each do |parameter|
-            if (:req == parameter.first || :opt == parameter.first)
-              fields << {
-                name: parameter[1],
-                type: :text
-              }
-            end
-          end
-          actions << {
-            name: state_transition.event,
-            class: 'transition',
-            href: server_device_url(@device.server, @device),
-            method: 'POST',
-            fields: fields
-          }
-        end
-        render zetta: @device,
-          links: links,
-          actions: actions,
-          class: ["device", @device.type],
-          properties: @device.attribute_names
+        render zetta: @device, controller: self
       }
     end  
   end
--- config/initializers/zetta.rb
+++ config/initializers/zetta.rb
@@ -2,36 +2,39 @@ require 'oat/adapters/siren'
 
 # TODO: create three serializers: root, server and device
 
-class ZettaSerializer < Oat::Serializer
+class DeviceSerializer < Oat::Serializer
   adapter Oat::Adapters::Siren
-
+  
   schema do
-    type *context[:class]
-    # TODO: fix the nested array on rel: [[up:, 'http://...']]
-    context[:links].each do |l| 
-      # link :self, :href => url_for(item.id)
-      link *l
-    end
-    context[:actions].each do |a| 
-      # link :self, :href => url_for(item.id)
-      action a[:name].to_sym do |action|
-        action.class a[:class]
-        action.href a[:href]
-        action.method a[:method]
-        action.type a[:type] if a[:type]
-        a[:fields].each do |field|
-          action.field field[:name].to_sym do |f|
-            f.type field[:type]
-            f.value field[:value] if field[:value]
-          end
-        end
-      end
-    end
-    map_properties *context[:properties]
+    device = item
+    device_controller = context[:controller]
+    
+    # TODO: DRY this up. why do I keep repeating device? block?
+    types device
+    links device, device_controller
+    properties device
   end
   
   def as_zetta(options)
     to_json
+  end
+  
+  protected
+  
+  def types(device)
+    type device.class.name.underscore, device.type
+  end
+  
+  def links(device, device_controller)
+    link :self, 
+      href: device_controller.server_device_url(device.server, device)
+    link [:up, 'http://rels.zettaapi.org/server'], 
+      href: device_controller.server_url(device.server), 
+      title: device.server.name
+  end
+  
+  def properties(device)
+    map_properties *device.attribute_names
   end
 
 end
@@ -39,5 +42,5 @@ end
 
 ActionController::Renderers.add :zetta do |resource, options|
   self.content_type ||= Mime[:zetta]
-  ZettaSerializer.new(resource, options).as_zetta(options)
+  DeviceSerializer.new(resource, options).as_zetta(options)
 end
