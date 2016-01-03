ad92a89145af96bdf888bd049612c3dace4a2621
landlessness <brian@landlessness.net>
2016-01-02 20:47:05 -0500

    device serializer in decent shape

--- config/initializers/zetta.rb
+++ config/initializers/zetta.rb
@@ -12,6 +12,7 @@ class DeviceSerializer < Oat::Serializer
     # TODO: DRY this up. why do I keep repeating device? block?
     types device
     links device, device_controller
+    actions device, device_controller
     properties device
   end
   
@@ -31,6 +32,27 @@ class DeviceSerializer < Oat::Serializer
     link [:up, 'http://rels.zettaapi.org/server'], 
       href: device_controller.server_url(device.server), 
       title: device.server.name
+  end
+  
+  def actions(device, device_controller)
+    device.state_transitions.each do |state_transition|
+      action state_transition.event do |action|
+        action.class 'transition'
+        action.href device_controller.server_device_url(device.server, device)
+        action.method 'POST'
+        action.field :action do |f|
+          f.type :hidden
+          f.value state_transition.event
+        end
+        device.class.instance_method(state_transition.event).parameters.each do |parameter|
+          if (:req == parameter.first || :opt == parameter.first)
+            action.field parameter[1].to_sym do |f|
+              f.type :text
+            end
+          end
+        end
+      end
+    end
   end
   
   def properties(device)
